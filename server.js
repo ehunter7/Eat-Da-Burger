a1edb9029c7022ff6166ccf69bb7e8d337f8b5d7
Eric Hunter <ehunter7@live.com>
2021-02-15 20:43:44 -0800

    adding express connection

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require(`express`);
 const exprhdlbs = require(`express-handlebars`);
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine(`handlebars`, exprhdlbs({ defaultLayout: `main` }));
+app.set(`view engine`, `handlebars`);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});

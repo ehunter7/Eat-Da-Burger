d45b93e64522fe945b953ab3d8eb77bb4c89b2c7
Eric Hunter <ehunter7@live.com>
2021-03-07 17:14:20 -0800

    added jaws db connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-
 require("dotenv").config();
 const mysql = require(`mysql`);
+
 if(process.env.JAWSDB_URL){
-  connection === mysql.createConnection(process.env.JAWSDB_URL)
+  const connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
-  connection = mysql.createConnection({
+  const connection = mysql.createConnection({
     host: `localhost`,
     port: 3306,
     user: `root`,
